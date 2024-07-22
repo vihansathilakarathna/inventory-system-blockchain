a1aa825485396cb15175ffd08308dad18478eb6f
Vihansa <vihansathilakrathne1999@gmail.com>
2024-07-22 15:19:17 +0530

    Summary Page design

--- frontend/src/Pages/Admin/ReportsPage.jsx
+++ frontend/src/Pages/Admin/ReportsPage.jsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React,{useState} from "react";
 import AdminNavbar from "../../Components/Navbar/AdminNavbar";
 import Reports from "../../Components/Admin/Reports/Reports";
 
 export default function ReportsPage() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const handleCollapseToggle = () => {
+    setIsCollapsed((prev) => !prev);
+  };
+
   const containerStyle = {
     display: "grid",
-    gridTemplateColumns: "250px 1fr",
+    gridTemplateColumns: isCollapsed ? "100px 1fr" : "200px 1fr",
     height: "100vh",
+    transition: "grid-template-columns 0.3s ease",
   };
 
   const contentStyle = {
-    padding: "80px 50px 50px 0",
+    marginLeft: isCollapsed ? "100px" : "200px",
+    transition: "margin-left 0.3s ease",
   };
   return (
-    <div>
-      <div style={containerStyle}>
-        <div>
-          <AdminNavbar />
-        </div>
-        <div style={contentStyle}>
-          <Reports />
-        </div>
+    <div style={containerStyle}>
+      <div>
+        <AdminNavbar onCollapseToggle={handleCollapseToggle} />
+      </div>
+      <div>
+        <Reports style={contentStyle} />
       </div>
     </div>
   );
